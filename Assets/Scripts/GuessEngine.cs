eaabf2dec26b2ed790c4b4a322861ed377a0af73
Laurie Trichet <laurie.trichet@gmail.com>
2019-02-04 23:03:13 +1100

    add project

---
+++ Assets/Scripts/GuessEngine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuessEngine : MonoBehaviour
+{
+
+    [SerializeField] TMPro.TMP_Text GuessText;
+    [SerializeField] int min = 1;
+    [SerializeField] int max = 1000;
+
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        GuessText.text = guess.ToString();
+    }
+
+    public void OnLower()
+    {
+        max = guess + 1;
+        NextGuess();
+    }
+
+    public void OnHigher()
+    {
+        min = guess - 1;
+        NextGuess();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        Debug.Log("on Load next scene");
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene((sceneIndex + 1) % 3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
